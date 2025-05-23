efd27d463c7550b4469d2d40210a28f95ebe929c
Vinicius <videpierri@gmail.com>
2025-05-23 15:53:17 -0300

    Criação da vpc e nacl

---
+++ terraform/desafio-vinicius/main.tf
@@ -0,0 +1,47 @@
+terraform {
+  required_version = "~>1.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+# Creating VPC
+module "vpc" {
+  source = "terraform-aws-modules/vpc/aws"
+
+  name = "vpc-desafio"
+  cidr = "10.0.0.0/16"
+
+  azs             = ["us-east-1a"]
+  public_subnets  = ["10.0.101.0/24"]
+
+  #NACL
+  default_network_acl_egress = [
+    {
+      "action" : "allow",
+      "cidr_block" : "0.0.0.0/0",
+      "from_port" : 0,
+      "protocol" : "-1",
+      "rule_no" : 100,
+      "to_port" : 0
+    }
+  ]
+  default_network_acl_ingress = [
+    {
+      "action" : "allow",
+      "cidr_block" : "0.0.0.0/0",
+      "from_port" : 0,
+      "protocol" : "-1",
+      "rule_no" : 100,
+      "to_port" : 0
+    }
+  ]
+  default_network_acl_name = "nacl_desafio"
+  default_network_acl_tags = {
+    Name = "nacl_desafio"
+  }
+}
+
---
+++ terraform/desafio-vinicius/variables.tf
@@ -0,0 +1,5 @@
+variable "provider_region" {
+  default     = "us-east-1"
+  type        = string
+  description = "Região que serão provisionados os recursos."
+}
